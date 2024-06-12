d6df819f3d9769afd6520ff9a90bf4327efe26ec
vasquezluis <luivasquez95@gmail.com>
2024-06-12 17:05:13 -0600

    feat: add CRUD endpoints

--- main.py
+++ main.py
@@ -1,13 +1,64 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import List, Optional
+from uuid import UUID, uuid4
 
 # instance of fastapi
 app = FastAPI()
 
 
-# basic route
-@app.get("/")
-def read():
-    return {"hello": "world"}
+# the model for task -> for the db
+class Task(BaseModel):
+    id: Optional[UUID] = None
+    title: str
+    description: Optional[str] = None
+    completed: bool = False
+
+
+# running db
+tasks = []
+
+
+@app.get("/tasks/", response_model=List[Task])
+def read_tasks():
+    return tasks
+
+
+@app.get("/tasks/{task_id}", response_model=Task)
+def read_task(task_id: UUID):
+    for task in tasks:
+        if task.id == task_id:
+            return task
+
+    raise HTTPException(status_code=404, detail="Task not found")
+
+
+@app.post("/tasks/", response_model=Task)
+def create_task(task: Task):
+    task.id = uuid4()
+    tasks.append(task)
+
+    return task
+
+
+@app.put("/tasks/{task_id}", response_model=Task)
+def update_task(task_id: UUID, task_update: Task):
+    for idx, task in enumerate(tasks):
+        if task.id == task_id:
+            updated_task = task.copy(update=task_update.model_dump(exclude_unset=True))
+            tasks[idx] = updated_task
+            return updated_task
+
+    raise HTTPException(status_code=404, detail="Task not found")
+
+
+@app.delete("/tasks/{task_id}", response_model=Task)
+def delete_task(task_id: UUID):
+    for idx, task in enumerate(tasks):
+        if task.id == task_id:
+            return tasks.pop(idx)
+
+    raise HTTPException(status_code=404, detail="Task not found")
 
 
 # execute the api
